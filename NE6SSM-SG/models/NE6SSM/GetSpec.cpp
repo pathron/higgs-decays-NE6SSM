ecc0e6f0afa5fe54b65c739fd51947f744813962
Peter Athron <peter.athron@adelaide.edu.au>
2014-07-11 21:25:06 +0930

    extended GetScale class lt help with scanning.

--- NE6SSM-SG/models/NE6SSM/GetSpec.cpp
+++ NE6SSM-SG/models/NE6SSM/GetSpec.cpp
@@ -1,7 +1,144 @@
 #include "GetSpec.hpp"
 
 using namespace flexiblesusy;
+
+void GetSpec::Output_slha(std::ostream& ostr) {
   
+   const Problems<NE6SSM_info::NUMBER_OF_PARTICLES>& problems
+      = spectrum_generator.get_problems();
+   
+   const std::string slha_output_file(cmd_line_options.get_slha_output_file());
+   const std::string spectrum_file(cmd_line_options.get_spectrum_file());
+   const std::string rgflow_file(cmd_line_options.get_rgflow_file());
+   // output
+   slha_io.set_spinfo(problems);
+   slha_io.set_sminputs(oneset);
+   slha_io.set_minpar(input);
+   slha_io.set_extpar(input);
+   if (!problems.have_serious_problem())
+      slha_io.set_spectrum(spectrum_generator.get_model());
+
+   if (slha_output_file.empty()) {
+      slha_io.write_to_stream(std::cout);
+   } else {
+      slha_io.write_to_file(slha_output_file);
+   }
+
+   if (!spectrum_file.empty())
+      spectrum_generator.write_spectrum(spectrum_file);
+
+   if (!rgflow_file.empty())
+      spectrum_generator.write_running_couplings(rgflow_file);
+
+   return;
+}
+
+
+void GetSpec::Output_slha(std::ostream& ostr, NE6SSM_input_parameters input_pars) {
+   //set internal input to match user adjusted one
+   input = input_pars;
+   //call routine to output to slha file using 
+   //private member input 
+   Output_slha(std::cout);
+   return;
+
+}
+
+int GetSpec::input_slha(int argc, const char* argv[]) {
+   ///make sure we start off with empty oneset
+   QedQcd newoneset;
+   oneset=newoneset;
+   //read in slha
+   Command_line_options options(argc, argv);
+   cmd_line_options = options;
+   if (options.must_print_model_info())
+      NE6SSM_info::print(std::cout);
+   if (options.must_exit())
+      return options.status();
+
+   const std::string slha_input_file(options.get_slha_input_file());
+   
+
+   if (slha_input_file.empty()) {
+      ERROR("No SLHA input file given!\n"
+            "   Please provide one via the option --slha-input-file=");
+      return EXIT_FAILURE;
+   }
+
+   try {
+      slha_io.read_from_file(slha_input_file);
+      slha_io.fill(oneset);
+      slha_io.fill(input);
+      slha_io.fill(spectrum_generator_settings);
+   } catch (const Error& error) {
+      ERROR(error.what());
+      return EXIT_FAILURE;
+   }
+   return 0;
+}
+int GetSpec::input_slha(int argc, const char* argv[],  NE6SSM_input_parameters & input_pars) {
+   //fill priate members from slha file
+   int exit_code = input_slha(argc,argv);
+   //set user passed input parameters to private member that was
+   //filled by slha
+   input_pars = input;
+   
+   return exit_code;
+}
+
+
+int GetSpec::getSpectrum(NE6SSM<Two_scale> & model) {
+  
+   oneset.toMz(); // run SM fermion masses to MZ
+
+   //   NE6SSM_spectrum_generator<algorithm_type> spectrum_generator;
+   spectrum_generator.set_precision_goal(
+      spectrum_generator_settings.get(Spectrum_generator_settings::precision));
+   spectrum_generator.set_max_iterations(
+      spectrum_generator_settings.get(Spectrum_generator_settings::max_iterations));
+   spectrum_generator.set_calculate_sm_masses(
+      spectrum_generator_settings.get(Spectrum_generator_settings::calculate_sm_masses) >= 1.0);
+   spectrum_generator.set_input_scale(
+      slha_io.get_input_scale());
+   spectrum_generator.set_parameter_output_scale(
+      slha_io.get_parameter_output_scale());
+   spectrum_generator.set_pole_mass_loop_order(
+      spectrum_generator_settings.get(Spectrum_generator_settings::pole_mass_loop_order));
+   spectrum_generator.set_ewsb_loop_order(
+      spectrum_generator_settings.get(Spectrum_generator_settings::ewsb_loop_order));
+   spectrum_generator.set_beta_loop_order(
+      spectrum_generator_settings.get(Spectrum_generator_settings::beta_loop_order));
+   spectrum_generator.set_threshold_corrections(
+      spectrum_generator_settings.get(Spectrum_generator_settings::threshold_corrections));
+
+   spectrum_generator.run(oneset, input);
+   //set model to pass to user
+   model = spectrum_generator.get_model();
+
+   const int exit_code = spectrum_generator.get_exit_code();
+  
+   return exit_code;
+
+}
+
+int GetSpec::getSpectrum(NE6SSM<Two_scale> & model, 
+                         NE6SSM_input_parameters  input_pars) {
+   input = input_pars;
+   getSpectrum(model);
+
+}
+
+int GetSpec::findSpectrum2(int argc, const char* argv[], 
+                            NE6SSM<Two_scale> & model, 
+                            NE6SSM_slha_io  & slha_io) { 
+   int read_ok = input_slha(argc, argv);
+   if(read_ok!=0) return read_ok;
+   int exit_code = getSpectrum(model);
+   Output_slha(std::cout);
+   
+   return exit_code;
+
+}
 int GetSpec::findSpectrum(int argc, const char* argv[], 
                             NE6SSM<Two_scale> & model, 
                             NE6SSM_slha_io  & slha_io) {
@@ -36,22 +173,7 @@ int GetSpec::findSpectrum(int argc, const char* argv[],
    } catch (const Error& error) {
       ERROR(error.what());
       return EXIT_FAILURE;
-   }
-
-   std::cout << "After reading in from SLHA file we now do scan." << std::endl;
-   cout << "# "
-        << std::setw(12) << std::left << "KapPr" << ' '
-        << std::setw(12) << std::left << "TKapPr" << ' '
-        << std::setw(12) << std::left << "Mhh(1)" << ' '
-        << std::setw(12) << std::left << "MAh(2)" << ' '
-        << std::setw(12) << std::left << "BR(h1-> A1 A1)"
-        << '\n';
-   for(int i = 0; i<= 1000; i++ ){
-      for(int j = 1; j<= 100; j++ ){
-         input.KappaPrInput = 5e-5 + i*1e-4;
-         input.TKappaPrInput = 1 + j;
-   
-  
+   }  
 
    oneset.toMz(); // run SM fermion masses to MZ
 
@@ -89,42 +211,20 @@ int GetSpec::findSpectrum(int argc, const char* argv[],
    if (!problems.have_serious_problem())
       slha_io.set_spectrum(model);
 
-   // if (slha_output_file.empty()) {
-   //    slha_io.write_to_stream(std::cout);
-   // } else {
-   //    slha_io.write_to_file(slha_output_file);
-   // }
+   if (slha_output_file.empty()) {
+      slha_io.write_to_stream(std::cout);
+   } else {
+      slha_io.write_to_file(slha_output_file);
+   }
 
-   // if (!spectrum_file.empty())
-   //    spectrum_generator.write_spectrum(spectrum_file);
+   if (!spectrum_file.empty())
+      spectrum_generator.write_spectrum(spectrum_file);
 
-   // if (!rgflow_file.empty())
-   //    spectrum_generator.write_running_couplings(rgflow_file);
+   if (!rgflow_file.empty())
+      spectrum_generator.write_running_couplings(rgflow_file);
 
    const int exit_code = spectrum_generator.get_exit_code();
 
-   const NE6SSM_physical& pole_masses = model.get_physical();
-   const double higgs = pole_masses.Mhh(0);
-   const double mA = pole_masses.MAh(2);
-   const double mHu2 = model.get_mHu2();
-   const double mHd2 = model.get_mHd2();
-   const double yt = model.get_Yu(2,2);
-   const double at = model.get_TYu(2,2);
-   const double mQ3sq = model.get_mq2(2,2);
-   const double mU3sq = model.get_mu2(2,2);
-    
-   // //this must be positive to evade colour or cahrge breaking minima
-   const double ccbfac =  Sqr(yt)*(mQ3sq + mU3sq + mHu2)- Sqr(at);
-   cout << "  "
-        << std::setw(12) << std::left <<model.get_KappaPr() << ' '
-        << std::setw(12) << std::left << model.get_TKappaPr()  << ' '
-        << std::setw(12) << std::left << ccbfac << ' '
-        << std::setw(12) << std::left << mHu2 << ' '
-        << std::setw(12) << std::left << mA << ' '
-        << std::setw(12) << std::left << higgs;
-
-      }
-   }
-   return 0;
+   return exit_code;
 
 }
--- NE6SSM-SG/models/NE6SSM/GetSpec.hpp
+++ NE6SSM-SG/models/NE6SSM/GetSpec.hpp
@@ -20,12 +20,42 @@ class GetSpec {
 
 private: 
    NE6SSM_spectrum_generator<Two_scale> spectrum_generator;
-
+   Spectrum_generator_settings spectrum_generator_settings;
+   NE6SSM_slha_io  slha_io;
+   NE6SSM_input_parameters input;
+   QedQcd oneset;
+   Command_line_options cmd_line_options;
+   
 public:
+   
+   ///fills private members slha_io, input and  
+   int input_slha(int argc, const char* argv[]);
+   ///fills private mebers and sets passed input for public use
+   int input_slha(int argc, const char* argv[],NE6SSM_input_parameters & inp);
+   ///oututs slha spectra to user specified ostream
+   void Output_slha(std::ostream& ostr);
+   ///sets input according to user passed object 
+   ///oututs slha spectra to user specified ostream 
+   void Output_slha(std::ostream& ostr, NE6SSM_input_parameters input_pars);
+   
+   
+   ///finds the spectrum after slha_io and input have been set   
+   int getSpectrum(NE6SSM<Two_scale> & model);
+   ///finds the spectrum for passed input_pars after spectrum has been read in  
+   int getSpectrum(NE6SSM<Two_scale> & model, NE6SSM_input_parameters inp);
+   
+   ///sets up from slha input file and does full spectrum calculation 
+   ///for the user but passes out a NE6SSM<Two_scale> and  NE6SSM_slha_io
+   ///object
    int findSpectrum(int argc, const char* argv[], NE6SSM<Two_scale> & model, 
                       NE6SSM_slha_io  & slha);
    
+   //as above but done by calling other routines so neater
+   int findSpectrum2(int argc, const char* argv[], NE6SSM<Two_scale> & model, 
+                     NE6SSM_slha_io  & slha_io);
+
 };
+
 
 } // namespace flexiblesusy
 
