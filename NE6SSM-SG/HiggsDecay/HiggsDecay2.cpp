0a0cbbe2d1f0cdb47d6dfb9c2fec039b4a0cba77
Peter Athron <peter.athron@coepp14.physics.adelaide.edu.au>
2014-07-14 04:56:01 +0930

    resolved conflicts in HiggsDecay2 that seemed to ap[pear from applying stash. but was after veryu strange git refusal to merge when it seemd like this was uptodate with respect to origin.

--- NE6SSM-SG/HiggsDecay/HiggsDecay2.cpp
+++ NE6SSM-SG/HiggsDecay/HiggsDecay2.cpp
@@ -261,45 +261,52 @@ bool scanMQ3MU3At(int argc, const char *argv[]){
    NE6SSM_slha_io slha;
    NE6SSM_input_parameters input_pars;
    int read = gs.input_slha(argc, argv,input_pars);
-  if(read != 0) {
-     std::cerr << "Fail reading SLHA file." <<std::endl;
-     return false;
-  }
-  gs.findSpectrum2(argc,argv,ne6ssm, slha);
-  double mHu2 =  ne6ssm.get_mHu2();
-  double yt =  ne6ssm.get_Yu(2,2);
 
-  double minMQ3sq=1.0e+05; double maxMQ3sq=1.0e+08; int stepsMQ3sq =30;
-  double stepMQ3sq = log(maxMQ3sq - minMQ3sq) / (stepsMQ3sq-1);
-  double minMU3sq=1.0e+05; double maxMU3sq=1.0e+08; int stepsMU3sq =10;
-  double stepMU3sq = log(maxMU3sq - minMU3sq) / (stepsMU3sq-1);
-  double minTYu=1000; double maxTYu= 3000; int stepsTYu =200;
-  double stepTYu = (maxTYu - minTYu) / (stepsTYu-1);
-  ofstream mHu2points;
-  mHu2points.open("mHu2points.dat");
-  for(int i=0; i<stepsMQ3sq; i++){
-     for(int j=0; j<stepsTYu; j++){
-        for(int k=0; k<stepsMU3sq; k++){
-           input_pars.mq2Input(2,2) = exp(log(minMQ3sq) + i * stepMQ3sq);
-           input_pars.TYuInput(2,2) = minTYu + j * stepTYu;
-           input_pars.mu2Input(2,2) = exp(log(minMU3sq) + i * stepMU3sq);
-           const double at =  input_pars.TYuInput(2,2);
-           const double mQ3sq = input_pars.mq2Input(2,2) ;
-           const double mU3sq =input_pars.mu2Input(2,2)  ;
+   if(read != 0) {
+      std::cerr << "Fail reading SLHA file." <<std::endl;
+      return false;
+   }
+   gs.findSpectrum2(argc,argv,ne6ssm, slha);
+   double mHu2 =  ne6ssm.get_mHu2();
+   double yt =  ne6ssm.get_Yu(2,2);
+   
+   //double minMQ3sq=6.0e+05; double maxMQ3sq=1.0e+07; int stepsMQ3sq =30;
+   double minMQ3sq=1.0e+05; double maxMQ3sq=1.0e+08; int stepsMQ3sq =20;
+   double stepMQ3sq = log(maxMQ3sq - minMQ3sq) / (stepsMQ3sq-1);
+   double minMU3sq=1.0e+05; double maxMU3sq=1.0e+08; int stepsMU3sq =30;
+   double stepMU3sq = log(maxMU3sq - minMU3sq) / (stepsMU3sq-1);
+   double minTYu=500; double maxTYu= 3000; int stepsTYu =300;
+   double stepTYu = (maxTYu - minTYu) / (stepsTYu-1);
+   ofstream mHu2points;
+   mHu2points.open("mHu2Neg_Atpos.dat");
+   for(int i=0; i<stepsMQ3sq; i++){
+      for(int j=0; j<stepsTYu; j++){
+         for(int k=0; k<stepsMU3sq; k++){
+            int read = gs.input_slha(argc, argv,input_pars);
+            if(read != 0) {
+               std::cerr << "Fail reading SLHA file." <<std::endl;
+               return false;
+            }
+            input_pars.mq2Input(2,2) = exp(log(minMQ3sq) + i * stepMQ3sq);
+            input_pars.TYuInput(2,2) = minTYu + j * stepTYu;
+            input_pars.mu2Input(2,2) = exp(log(minMU3sq) + i * stepMU3sq);
+            const double at =  input_pars.TYuInput(2,2);
+            const double mQ3sq = input_pars.mq2Input(2,2) ;
+            const double mU3sq =input_pars.mu2Input(2,2)  ;
             //this must be positive to evade colour or cahrge breaking minima
-           //use 1e+06 for mHu2 to be conservative here.
-           double ccbfac =  Sqr(yt)*(mQ3sq + mU3sq+mHu2)- Sqr(at);
-           if(ccbfac < 0) continue;
-           int exit_code = gs.getSpectrum(ne6ssm, input_pars);
-           if(exit_code != 0) continue;
-           double BRhA1A1 = higgs_decay_example(ne6ssm,false);
-           const NE6SSM_physical& pole_masses = ne6ssm.get_physical();
-           //update with correct yt and mHu2
-           mHu2 =  ne6ssm.get_mHu2();
-           yt =  ne6ssm.get_Yu(2,2);
-            ccbfac =  Sqr(yt)*(mQ3sq + mU3sq+mHu2)- Sqr(at);
-    
-           if(mHu2 < 0){    
+            //use 1e+06 for mHu2 to be conservative here.
+            double ccbfac =  3.0*Sqr(yt)*(mQ3sq + mU3sq + mHu2)- Sqr(at);
+            if(ccbfac < 0) continue;
+            int exit_code = gs.getSpectrum(ne6ssm, input_pars);
+            if(exit_code != 0) continue;
+            double BRhA1A1 = higgs_decay_example(ne6ssm,false);
+            const NE6SSM_physical& pole_masses = ne6ssm.get_physical();
+            //update with correct yt and mHu2
+            mHu2 = ne6ssm.get_mHu2();
+            yt = ne6ssm.get_Yu(2,2);
+            ccbfac =  3.0*Sqr(yt)*(mQ3sq + mU3sq + mHu2)- Sqr(at);
+            
+            if(mHu2 < 0){    
               mHu2points << "  "
                          << std::setw(12) << std::left <<mQ3sq << ' ' 
                          << std::setw(12) << std::left <<mU3sq << ' '
@@ -308,7 +315,10 @@ bool scanMQ3MU3At(int argc, const char *argv[]){
                          << std::setw(12) << std::left << mHu2 << ' '
                          << std::setw(12) << std::left << pole_masses.MAh(2) << ' '
                          << std::setw(12) << std::left <<  pole_masses.Mhh(0)<< ' '
-                         << std::setw(12) << std::left << BRhA1A1 << std::endl;
+                         << std::setw(12) << std::left << BRhA1A1 
+			 << std::setw(12) << std::left <<log(mQ3sq) << ' ' 
+                         << std::setw(12) << std::left <<log(mU3sq) << ' '
+			 << std::endl;
            }
            else{
               cout << "  "   
